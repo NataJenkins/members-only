bf8578c60c54cd7852196b2408747e8d149f67c3
Nexch <mauricio.t.1999@gmail.com>
2020-12-14 10:49:03 -0500

    fix rubocop errors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,9 @@
 module PostsHelper
   def check
-      @posts.each do |post|
-        concat content_tag(:tr,content_tag(:td,post.post) + content_tag(:td,(post.user.email if user_signed_in? )) + content_tag(:td,(link_to 'Show', post)) + content_tag(:td,(content_tag(:td,(link_to 'Show', post)))) + content_tag(:td,(link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }))
-)
-      end
+    @posts.each do |post|
+      concat content_tag(:tr, content_tag(:td, post.post) + content_tag(:td, (if user_signed_in?
+                                                                                post.user.email
+                                                                              end)) + content_tag(:td, (link_to 'Show', post)) + content_tag(:td, content_tag(:td, (link_to 'Show', post))) + content_tag(:td, (link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' })))
+    end
   end
 end
