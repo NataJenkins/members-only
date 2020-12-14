710a9cd0a825e94c3573d383ea7e657a475faaad
Nexch <mauricio.t.1999@gmail.com>
2020-12-14 10:33:59 -0500

    fix errors in the views and in the routes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  def check
+      @posts.each do |post|
+        concat content_tag(:tr,content_tag(:td,post.post) + content_tag(:td,(post.user.email if user_signed_in? )) + content_tag(:td,(link_to 'Show', post)) + content_tag(:td,(content_tag(:td,(link_to 'Show', post)))) + content_tag(:td,(link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }))
+)
+      end
+  end
 end
