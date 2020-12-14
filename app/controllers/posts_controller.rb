7b368293d9599cf718d80a33fcf41e28d233c116
Nexch <mauricio.t.1999@gmail.com>
2020-12-14 02:11:48 -0500

    added author name only appears when log in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except:[:new, :show, :show]
+  before_action :authenticate_user!, except:[:new, :show, :index]
 
   # GET /posts
   # GET /posts.json
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201211153345) do
+ActiveRecord::Schema.define(version: 20201214064400) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "user"
   end
 
   create_table "users", force: :cascade do |t|
